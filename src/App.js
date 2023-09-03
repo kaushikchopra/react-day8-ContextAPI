863afd78b80e8e28ee224c439855424e1a5a41d5
S Kaushik Chopra <kaushikchopra2908@gmail.com>
2023-09-03 14:45:06 +0530

    Cart page using Context API completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ProductList from "./Components/ProductList";
+import Navbar from "./Components/Navbar";
+import { Routes, Route } from 'react-router-dom';
+import CartState from './Context/CartState'
+import Cart from "./Components/Cart";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CartState>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </CartState>
+      <Footer />
+    </>
   );
 }
 
